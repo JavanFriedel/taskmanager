33a63546650d35895fc720be5530ddb32214ba13
JavanFriedel <javanfriedel@hotmail.com>
2022-03-21 20:37:31 -0400

    Fixed Air BnB eslint errors, fixed prettier config to match

--- src/test.js
+++ src/test.js
@@ -1,48 +1,90 @@
-//Base Data Object for all projects and tasks
+// Base Data Object for all projects and tasks
 const Data = {};
 
 let activeProject;
 
 // Caching dom referrences
-const projectList = document.querySelector("nav");
-const projectAddNode = document.querySelector(".addProject");
+const projectList = document.querySelector('nav');
+// const projectAddNode = document.querySelector(".addProject");
 
-const taskView = document.querySelector(".project-content");
-const taskAddNode = document.querySelector(".addTask");
+const taskView = document.querySelector('.project-content');
+// const taskAddNode = document.querySelector('.addTask');
+
+function wipeTaskView() {
+  while (taskView.childNodes.length > 1) {
+    taskView.removeChild(taskView.firstChild);
+  }
+}
+
+// render DOM node for task
+function renderTaskNode(task) {
+  const taskNode = document.createElement('div');
+  taskNode.classList.add('taskContainer');
+
+  taskNode.innerText = task.title; // TEMPORARY
+
+  // projectList.insertBefore(projectTab, projectAddNode);
+  taskView.insertBefore(taskNode, taskView.firstChild);
+}
+
+// Create a task node for each task listed
+function renderTasks(project) {
+  const keys = Object.keys(project.tasks);
+
+  keys.forEach((task) => {
+    renderTaskNode(project.tasks[task]);
+  });
+}
+
+// Dont know what im doing with this yet
+function renderProjectView(project) {
+  activeProject = Data[project];
+  renderTasks(project);
+}
+
+function changeProject(project) {
+  activeProject = project;
+  console.log(activeProject);
+
+  document.querySelector('.header > .title').innerText = activeProject.title;
+
+  wipeTaskView();
+  renderProjectView(project);
+}
 
 // --- Add Project to Nav ---
 function addProjectTab(obj) {
-  const projectTab = document.createElement("div");
-  projectTab.classList.add("nav-project");
-  projectTab.setAttribute(`data-project`, obj.title);
+  const projectTab = document.createElement('div');
+  projectTab.classList.add('nav-project');
+  projectTab.setAttribute('data-project', obj.title);
 
-  const projectTitle = document.createElement("div");
-  projectTitle.classList.add("title");
+  const projectTitle = document.createElement('div');
+  projectTitle.classList.add('title');
   projectTitle.innerText = obj.title;
 
-  const projectOptions = document.createElement("div");
-  projectOptions.classList.add("project-options");
-  projectOptions.innerText = "... E X"; //TEMPORARY
+  const projectOptions = document.createElement('div');
+  projectOptions.classList.add('project-options');
+  projectOptions.innerText = '... E X'; // TEMPORARY
 
-  projectTab.addEventListener("click", (event) => {
+  projectTab.addEventListener('click', (event) => {
     let dataAtrribute;
 
-    if (event.target == projectTitle) {
-      dataAtrribute = event.target.parentNode.getAttribute("data-project");
+    if (event.target === projectTitle) {
+      dataAtrribute = event.target.parentNode.getAttribute('data-project');
     } else {
-      dataAtrribute = event.target.getAttribute("data-project");
+      dataAtrribute = event.target.getAttribute('data-project');
     }
 
     changeProject(Data[dataAtrribute]);
   });
 
   projectTab.append(projectTitle, projectOptions);
-  projectList.insertBefore(projectTab, projectAddNode);
+  projectList.append(projectTab);
 }
 
 // - Factory: Initalize Project Obj
 function createProject(title, dueDate, priority, color, tasks) {
-  let taskObj = {};
+  const taskObj = {};
 
   taskObj[tasks.title] = tasks;
 
@@ -62,31 +104,16 @@ function appendProject(obj) {
 
 // Uses Helper function to Inject project data and render dom
 function newProject(title, dueDate, priority, color, tasks) {
-  let project = createProject(title, dueDate, priority, color, tasks);
+  const project = createProject(title, dueDate, priority, color, tasks);
   appendProject(project);
 }
 
 // Render Project Data to DOM
 function renderProjects() {
-  for (project in Data) {
+  const keys = Object.keys(Data);
+  keys.forEach((project) => {
     addProjectTab(Data[project]);
-  }
-}
-
-function changeProject(project) {
-  activeProject = project;
-  console.log(activeProject);
-
-  document.querySelector(".header > .title").innerText = activeProject.title;
-
-  wipeTaskView();
-  renderProjectView(project);
-}
-
-function wipeTaskView() {
-  while (taskView.childNodes.length > 1) {
-    taskView.removeChild(taskView.firstChild);
-  }
+  });
 }
 
 // --- TASK MANAGEMENT ---
@@ -102,51 +129,27 @@ function createTaskObj(title, desc, dueDate, priority, notes) {
   };
 }
 
-//render DOM node for task
-function renderTaskNode(task) {
-  const taskNode = document.createElement("div");
-  taskNode.classList.add("taskContainer");
-
-  taskNode.innerText = task.title; //TEMPORARY
-
-  // projectList.insertBefore(projectTab, projectAddNode);
-  taskView.insertBefore(taskNode, taskView.firstChild);
-}
-
-// Create a task node for each task listed
-function renderTasks(project) {
-  for (task in project.tasks) {
-    renderTaskNode(project.tasks[task]);
-  }
-}
-
 // Append new Task
 function appendTask(task) {
   activeProject.tasks[task.title] = task;
 }
 
-// Dont know what im doing with this yet
-function renderProjectView(project) {
-  activeProject = Data[project];
-  renderTasks(project);
-}
-
 // Injecting Test Data
-let testTask = createTaskObj("Task1", "This is test", "X", "X", "X");
-let testTask2 = createTaskObj("Task2", "This is test", "X", "X", "X");
-let testTask3 = createTaskObj("Task3", "This is test", "X", "X", "X");
-newProject("Default", "tomorrow", "high", "#FF", testTask);
-newProject("Its working!", "asdas", "hiaasdgh", "#FFFF", testTask);
+const testTask = createTaskObj('Task1', 'This is test', 'X', 'X', 'X');
+const testTask2 = createTaskObj('Task2', 'This is test', 'X', 'X', 'X');
+const testTask3 = createTaskObj('Task3', 'This is test', 'X', 'X', 'X');
+newProject('Default', 'tomorrow', 'high', '#FF', testTask);
+newProject('Its working!', 'asdas', 'hiaasdgh', '#FFFF', testTask);
 
 renderProjects();
 
-//set initial active Project
-activeProject = Data["Default"];
-console.log("Active Project:" + activeProject);
+// set initial active Project
+activeProject = Data.Default;
+console.log(`Active Project:${activeProject}`);
 appendTask(testTask2);
 appendTask(testTask3);
 
-renderTasks(Data["Default"]);
+renderTasks(Data.Default);
 
 // changeProject(Data["Its working!"]);
 
